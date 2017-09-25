9a506510b334b1d69aa624c8ffc95a8a671f0dc3
logan <logan70@163.com>
2017-09-25 23:26:03 +0800

    config autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -26,7 +26,7 @@ var gulp = require('gulp'), // 必须先引入gulp插件
 gulp.task('sass', function() {
   return sass('src/sass/**/*.scss', {style: 'expanded'})  // 传入 sass 目录及子目录下的所有 .scss 文件生成文件流通过管道并设置输出格式
     .pipe(cached('sass'))  // 缓存传入文件，只让已修改的文件通过管道（第一次执行是全部通过，因为还没有记录缓存）
-    .pipe(autoprefixer('last 6 version')) // 添加 CSS 浏览器前缀，兼容最新的5个版本
+    .pipe(autoprefixer('last 100 version')) // 添加 CSS 浏览器前缀，兼容最新的5个版本
     .pipe(gulp.dest('dist/css')) // 输出到 dist/css 目录下（不影响此时管道里的文件流）
     .pipe(rename({suffix: '.min'})) // 对管道里的文件流添加 .min 的重命名
     .pipe(cssnano()) // 压缩 CSS
